8f1e3f3b942ca118f8729f0fae606e7b91fb7115
Young Jun Park <june0313@gmail.com>
2020-03-22 19:33:03 +0900

    Initial commit

---
+++ src/main/java/org/jun/todo/TodoApplication.java
@@ -0,0 +1,13 @@
+package org.jun.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
+
+}
